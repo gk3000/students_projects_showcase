db6919a6549b17f85a71dd2f9b5c6fe104e85d7e
GK <george@barcelonacodeschool.com>
2022-02-25 16:23:42 +0100

    auto push

--- src/App.js
+++ src/App.js
@@ -498,7 +498,7 @@ export default function App() {
 <p>Audio guide app for visitors</p>
 </div>
 <div className='imgBackground'>
-<a href='https://www.bretthalperin.com/cccb.html' target='_blank' rel="noopener noreferrer">
+<a href='https://vimeo.com/376875142' target='_blank' rel="noopener noreferrer">
 <img src={CCCB} alt='CCCB, Audio guide app for visitors' />
 </a>
 </div>
