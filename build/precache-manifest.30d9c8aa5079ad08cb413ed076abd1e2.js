bec4c579859e5a3350d14967bef1644749ac60da
gk3000 <george@barcelonacodeschool.com>
2024-05-13 11:15:09 +0200

    16 build

---
+++ build/precache-manifest.30d9c8aa5079ad08cb413ed076abd1e2.js
@@ -0,0 +1,298 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a8d05784e19aaf52425ce3e658c4d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d0fb97ecd058f31d2c4",
+    "url": "/static/css/main.9b4c4f19.chunk.css"
+  },
+  {
+    "revision": "a762477e57906900f02b",
+    "url": "/static/js/2.600e38a7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.600e38a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d0fb97ecd058f31d2c4",
+    "url": "/static/js/main.b5e62bb0.chunk.js"
+  },
+  {
+    "revision": "92cb8b11782128949ed7",
+    "url": "/static/js/runtime-main.4fa9208a.js"
+  },
+  {
+    "revision": "8d3a0088457db73cf8d0dc8ee72f53d0",
+    "url": "/static/media/80-20.8d3a0088.jpg"
+  },
+  {
+    "revision": "8c39a047980547a3bcaf3eaca34a5a6b",
+    "url": "/static/media/Neumorphism.8c39a047.png"
+  },
+  {
+    "revision": "53adc3a90ac878db4369f59c5521e474",
+    "url": "/static/media/Weatherjokes.53adc3a9.jpg"
+  },
+  {
+    "revision": "1f71e465eb285d7ea594d2affa9bf691",
+    "url": "/static/media/amdb.1f71e465.jpg"
+  },
+  {
+    "revision": "ee61a510fac91035a2b3c59ab854354f",
+    "url": "/static/media/amplio.ee61a510.jpg"
+  },
+  {
+    "revision": "ffd5bc77dc1a9bf592e0db746d890f38",
+    "url": "/static/media/awen.ffd5bc77.png"
+  },
+  {
+    "revision": "42ac195af5819a7543ccf8d7c3dc8df7",
+    "url": "/static/media/beaktiv.42ac195a.jpg"
+  },
+  {
+    "revision": "602126a3c65b8d7090bda4229a9a0c09",
+    "url": "/static/media/booker.602126a3.jpg"
+  },
+  {
+    "revision": "d2a2a8b9c5a6bde3bbd96ef8fd74b9a4",
+    "url": "/static/media/bookily.d2a2a8b9.jpg"
+  },
+  {
+    "revision": "c9d78ed420533400b91971c091a40ae5",
+    "url": "/static/media/bookworld_anders.c9d78ed4.jpg"
+  },
+  {
+    "revision": "b6007959adfa611168286d2de8f410b5",
+    "url": "/static/media/boring-weather.b6007959.jpg"
+  },
+  {
+    "revision": "b81c240b32537e233e93d3eea9dd075b",
+    "url": "/static/media/brewit.b81c240b.jpg"
+  },
+  {
+    "revision": "5a9b31c59b4596ae4fac708fb4968c9d",
+    "url": "/static/media/buddiez.5a9b31c5.jpg"
+  },
+  {
+    "revision": "1cc611660ec8fda566c400a0eaec7854",
+    "url": "/static/media/cccb_brett.1cc61166.jpg"
+  },
+  {
+    "revision": "16b35c8b035504ed1dc34c64f7965acc",
+    "url": "/static/media/charles.16b35c8b.jpg"
+  },
+  {
+    "revision": "436ac1fbaf5441703a4ed463e1b8697c",
+    "url": "/static/media/complex-app.436ac1fb.png"
+  },
+  {
+    "revision": "965e7fd36b5cb5a1b58a7db1fc26af98",
+    "url": "/static/media/cupra.965e7fd3.png"
+  },
+  {
+    "revision": "6e006d8943e2a6d3fa1ff42d2ea54bf0",
+    "url": "/static/media/esperanca_screen.6e006d89.jpg"
+  },
+  {
+    "revision": "48d9a650f821605d99b64d07e9509a83",
+    "url": "/static/media/findjob.48d9a650.jpg"
+  },
+  {
+    "revision": "67483f4ddee4e8b5656052972f69800b",
+    "url": "/static/media/gabriella.67483f4d.png"
+  },
+  {
+    "revision": "3043c082b32be9206cf479280c335e97",
+    "url": "/static/media/gervie.3043c082.png"
+  },
+  {
+    "revision": "92fa85daed6089322a5e8d5bb3f7adbe",
+    "url": "/static/media/huisjein.92fa85da.jpg"
+  },
+  {
+    "revision": "1bbfd0124e1de3d119b26a2538ec1a0c",
+    "url": "/static/media/josh_weather.1bbfd012.png"
+  },
+  {
+    "revision": "3637c9c3fee8078078529db1e94be2a9",
+    "url": "/static/media/kofi.3637c9c3.png"
+  },
+  {
+    "revision": "eeb3a383c91c32efd25b2432261d47de",
+    "url": "/static/media/levelup.eeb3a383.jpg"
+  },
+  {
+    "revision": "2135e396f3d0fd9134b9b60d77c669bf",
+    "url": "/static/media/littleBird.2135e396.jpg"
+  },
+  {
+    "revision": "86963b7e7a56e8838c1900829e4f6c56",
+    "url": "/static/media/localu.86963b7e.png"
+  },
+  {
+    "revision": "c0855441de9bd3297ce94b65b2380ba9",
+    "url": "/static/media/magda-ecommerce.c0855441.jpg"
+  },
+  {
+    "revision": "4f146b1f2632b67d3031d9be6d78742d",
+    "url": "/static/media/martin_crypto.4f146b1f.jpg"
+  },
+  {
+    "revision": "7e08f39a9df4d74f5147ad10724d680d",
+    "url": "/static/media/michele_corona.7e08f39a.png"
+  },
+  {
+    "revision": "907b1e7a366152e56b8ac62ceafdefcf",
+    "url": "/static/media/moviexdb-netlify-app.907b1e7a.jpg"
+  },
+  {
+    "revision": "4a03d06cf47c828eb757cdcf1f9beb94",
+    "url": "/static/media/nebulosity.4a03d06c.jpg"
+  },
+  {
+    "revision": "b16c54ac996e3ae66313911cb64e6b1e",
+    "url": "/static/media/nutrition-and-calories-tracker.b16c54ac.jpg"
+  },
+  {
+    "revision": "247b4de22038fa1f41a7a3109480f7ae",
+    "url": "/static/media/one_of_us.247b4de2.jpg"
+  },
+  {
+    "revision": "391e1fdc7aedff1ebf8aca4972627961",
+    "url": "/static/media/pandora_ecommerce.391e1fdc.jpg"
+  },
+  {
+    "revision": "8c3ba2f62b781a478512dd8241014c87",
+    "url": "/static/media/pantry_buddy.8c3ba2f6.jpg"
+  },
+  {
+    "revision": "83f9b9ad623eaae0a1ee1e8503918556",
+    "url": "/static/media/pi-weather.83f9b9ad.jpg"
+  },
+  {
+    "revision": "5870a8760eea0c6983cfee9a7c50ec04",
+    "url": "/static/media/plateo.5870a876.jpg"
+  },
+  {
+    "revision": "1f610c336ff12a65fd9800365bc7be66",
+    "url": "/static/media/playground.1f610c33.jpg"
+  },
+  {
+    "revision": "b33efc22d83724ead79614c4f4eb43d8",
+    "url": "/static/media/plus_food_julieta.b33efc22.jpg"
+  },
+  {
+    "revision": "334141c24729731f518fd83157e58cf2",
+    "url": "/static/media/projects-barcelonacodeschool-billies-beer.334141c2.jpg"
+  },
+  {
+    "revision": "f2e07273302369aa2748d829efe16906",
+    "url": "/static/media/projify.f2e07273.png"
+  },
+  {
+    "revision": "2e5961bbc9c85957e84e9a2d547371bf",
+    "url": "/static/media/rainbow-beetroot.2e5961bb.jpg"
+  },
+  {
+    "revision": "cb032e33cfdf7347e71d7dd72e4063c3",
+    "url": "/static/media/read-a-book.cb032e33.png"
+  },
+  {
+    "revision": "358541debb3b27bb42ffce3e211cf5c1",
+    "url": "/static/media/rechargenow.358541de.jpg"
+  },
+  {
+    "revision": "fc7ec6173addd28ffd2c664881d2cdec",
+    "url": "/static/media/rooftop_guide.fc7ec617.png"
+  },
+  {
+    "revision": "c1a90eac5a8ca6f1ac928db480220a87",
+    "url": "/static/media/sara.c1a90eac.jpg"
+  },
+  {
+    "revision": "0d3a88f9f1a4c3df42c83ad7ac6c95f5",
+    "url": "/static/media/sba.0d3a88f9.png"
+  },
+  {
+    "revision": "05431354a28b8b4ed904ce12a218327e",
+    "url": "/static/media/soulfood.05431354.jpg"
+  },
+  {
+    "revision": "309d8652077a4605ff2c604ee7b04e99",
+    "url": "/static/media/styleship.309d8652.jpg"
+  },
+  {
+    "revision": "15cdb90b2fd88673f0b2ad32e91f4e4a",
+    "url": "/static/media/subsala.15cdb90b.jpg"
+  },
+  {
+    "revision": "b137aeb13ebc40711e80ab48ca2fc69a",
+    "url": "/static/media/swapp.b137aeb1.jpg"
+  },
+  {
+    "revision": "966e7c1ca5cb3c82df0710552942a952",
+    "url": "/static/media/tbot.966e7c1c.png"
+  },
+  {
+    "revision": "99e34899b5fba53543357d9843f35d55",
+    "url": "/static/media/the_gallows.99e34899.jpg"
+  },
+  {
+    "revision": "b0b7ac1a6dd6390ce0246a824b2bc8b4",
+    "url": "/static/media/tommy-commerce.b0b7ac1a.jpg"
+  },
+  {
+    "revision": "93fefbb2b29b8f26510fa2f8493c4574",
+    "url": "/static/media/trang.93fefbb2.png"
+  },
+  {
+    "revision": "866bfd57c91cf7753cfe2f855adbbe8b",
+    "url": "/static/media/treatyoself.866bfd57.png"
+  },
+  {
+    "revision": "42f4d0a2310e8f9a88ce36ef3a4a5b73",
+    "url": "/static/media/treviso.42f4d0a2.jpg"
+  },
+  {
+    "revision": "627b4f70aef2b77ac484c1a2b7aa0fd6",
+    "url": "/static/media/turtle.627b4f70.jpg"
+  },
+  {
+    "revision": "bf8af77bd960401e0ebd3b4897969fa5",
+    "url": "/static/media/useitup.bf8af77b.jpg"
+  },
+  {
+    "revision": "bf474df7690fc92fe6bc36bf34adcb4a",
+    "url": "/static/media/vietnamtours.bf474df7.png"
+  },
+  {
+    "revision": "f0a169183daf5379fb8a8393df43ae53",
+    "url": "/static/media/vinatt.f0a16918.jpg"
+  },
+  {
+    "revision": "7334e754e97807e5946fef2e3cb16c62",
+    "url": "/static/media/volya.7334e754.jpg"
+  },
+  {
+    "revision": "82469ebff60b97f73462a9dea47c93f6",
+    "url": "/static/media/walk_n_talk.82469ebf.jpg"
+  },
+  {
+    "revision": "b2f17d9dac1c2b53c37141d4fcd7bdc3",
+    "url": "/static/media/wander_woman.b2f17d9d.png"
+  },
+  {
+    "revision": "15e0b2108d9eaeb117df444eb0d44bcb",
+    "url": "/static/media/wavey.15e0b210.png"
+  },
+  {
+    "revision": "95a1a4019c7c170c80eda7f9093f8bdd",
+    "url": "/static/media/winemind.95a1a401.jpg"
+  },
+  {
+    "revision": "7ab1b41981797228a8598a71c1a7a158",
+    "url": "/static/media/yield.7ab1b419.jpg"
+  }
+]);
